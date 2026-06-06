1c3405749b4c1d148e8b7c1070af2609bc5b3944
Nilckson Getakwa <136057744+Nilckson@users.noreply.github.com>
2026-06-06 19:59:17 +0300

    Import Link component from next/link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { articles } from "./data/articles";
