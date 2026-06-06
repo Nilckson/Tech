78984782e70488f0fdbd675c70c4b259bdbdb595
Nilckson Getakwa <136057744+Nilckson@users.noreply.github.com>
2026-06-06 09:42:03 +0300

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>nilckson.tech is Live!</h1>
+      <p>Next.js deployment successful.</p>
+    </main>
+  );
+}
