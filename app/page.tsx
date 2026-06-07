a989080615a2f5829212c67bb18fa11f40880b56
Nilckson Getakwa <136057744+Nilckson@users.noreply.github.com>
2026-06-07 13:53:09 +0300

    Fix import path for SocialBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import SocialBar from "../components/SocialBar";
+import SocialBar from "./components/SocialBar";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
